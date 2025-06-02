dee1901c6bc86357f96bd6e439539c8f7cd579f4
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-06-02 09:20:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,64 @@
 import React from "react";
 import axios from "axios";
 import './../styles/App.css';
-
+import React, { useState } from "react";
 const App = () => {
-  const fetchUser = async () =>{
-    try{
-      axios.get("https://reqres.in/api/users");
-    }catch(err){
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchUser = async () => {
+    console.log("fetching .....");
+    try {
+      const res = await fetch("https://reqres.in/api/users", {
+        headers: {
+          "x-api-key": "reqres-free-v1",
+        },
+      });
+      const json = await res.json();
+      setData(json.data);
+      setIsLoading(false);
+    } catch (err) {
       console.error(err.message);
     }
-    
-  } 
-  // useEffect(() =>{
-  //   fetchUser();
-  // },[])
+  };
+  console.log(data);
+
   return (
     <div>
-       <button onClick={fetchUser} > Click Me</button>
-     
+      <div>
+        <h3>Blue Whales</h3>
+        <button className="btn" onClick={fetchUser}>
+          {" "}
+          Get User List
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading && <p>No data found to display.</p>}
+          {data &&
+            data.map((info) => {
+              return (
+                <tr>
+                  <td>{info.first_name}</td>
+                  <td>{info.last_name}</td>
+                  <td>{info.email}</td>
+                  <td>
+                    <img src={info.avatar} alt="user" />
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
